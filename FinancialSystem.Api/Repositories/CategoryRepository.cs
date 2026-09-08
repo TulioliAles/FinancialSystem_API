1927593a47e1fa8e2fc4fd4ebf4204984da90062
Alessandro Tulioli <desmecad@hotmail.com>
2026-09-08 16:02:59 -0300

    Criação do Repository de Category

---
+++ FinancialSystem.Api/Repositories/CategoryRepository.cs
@@ -0,0 +1,33 @@
+﻿using Dapper;
+using FinancialSystem.Api.Models;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace FinancialSystem.Api.Repositories
+{
+    public class CategoryRepository : ICategoryRepository
+    {
+        private readonly string _connectionString;
+
+        public CategoryRepository(IConfiguration configuration)
+        {
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+
+        private IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+        public async Task<IEnumerable<Category>> GetAllAsync()
+        {
+            using var conn = CreateConnection();
+            var sql = "SELECT * FROM Categories";
+            return await conn.QueryAsync<Category>(sql);
+        }
+
+        public async Task<Category?> GetByIdAsync(int id)
+        {
+            using var conn = CreateConnection();
+            var sql = "SELECT * FROM Categories WHERE Id = @Id";
+            return await conn.QueryFirstOrDefaultAsync<Category>(sql, new { Id = id});
+        }
+    }
+}
---
+++ FinancialSystem.Api/Repositories/ICategoryRepository.cs
@@ -0,0 +1,10 @@
+﻿using FinancialSystem.Api.Models;
+
+namespace FinancialSystem.Api.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(int id);
+    }
+}
